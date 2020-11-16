7157de96d627d7db127d35b5269e25569976ebb9
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2020-11-16 20:01:56 +0300

    Add links to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,18 +2,19 @@ import './header.css';
 import React from 'react';
 
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 
 const Header = ({numitems, total}) => (
         <header className="header">
-            <span className="header__logo">ReStore</span>
-            <div className="header__cart">
+            <Link to="/" className="header__logo">ReStore</Link>
+            <Link to="/cart" className="header__cart">
                 <ShoppingCartIcon color="primary"/>
                 <div className="header__cart-wrp">
                     <span>{numitems} items</span>
                     <span>{total}$</span>
                 </div>
-            </div>
+            </Link>
         </header>
 )
 
