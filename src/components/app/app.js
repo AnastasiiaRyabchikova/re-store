5168f37d6500f40d31062e46da6c31de06260f88
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-01-08 12:12:05 +0300

    @/components: add data to Header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../header';
+import { connect } from 'react-redux';
 
 import { Route, Switch } from 'react-router-dom';
 import { HomePage, CartPage } from '../pages';
-const App = () => {
+const App = ({ items }) => {
+    const totalCalc = items.reduce((accum, { price, count }) => (accum + count * price), 0);
     return (
         <React.Fragment>
-            <Header numitems={5} total={500}/>
+            <Header numitems={items.length} total={totalCalc}/>
             <Switch>
                 <Route path="/" component={HomePage} exact/>
                 <Route path="/cart" component={CartPage} />
@@ -15,4 +17,10 @@ const App = () => {
     )
 }
 
-export default App;
+const mapStateToProps = ({ shopingCart: { cartItems } }) => {
+    return {
+        items: cartItems,
+    }
+};
+
+export default connect(mapStateToProps)(App);
